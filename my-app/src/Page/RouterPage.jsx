a2108380b378ea0bcc7073eb4e08404f634c4386
zwon2056 <zwon2056@gmail.com>
2022-06-29 12:21:02 +0900

    LOAD: 바꾼거 저장 안해둬서 새로 저장

--- my-app/src/Page/RouterPage.jsx
+++ my-app/src/Page/RouterPage.jsx
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import Test from '../Component/Test';
-import Test2 from '../Component/Test2';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function RouterPage() {
